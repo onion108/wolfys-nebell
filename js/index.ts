836742b1e3e4ee5f59070dee4cd1aa73425a58db
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-04-03 09:38:17 +0800

    Qingming update

--- js/index.ts
+++ js/index.ts
@@ -7,9 +7,15 @@ function ziyueMode() {
 
 window.onload = () => {
     let dateNow = new Date();
+    let ねん = dateNow.getFullYear()
     let げつ = dateNow.getMonth();
     let にち = dateNow.getDate();
     if (げつ == 2 && にち == 5) {
         ziyueMode()
+    }
+    if (ねん == 2022 && げつ == 3 && (にち >= 3 && にち <= 10)) {
+        ziyueMode()
+        document.getElementById('an_t').innerHTML = "清明节纪念袁蒋梓悦";
+        document.getElementById('an_c').innerHTML = "本网站默哀一周，请在此纪念她。"
     }
 }
